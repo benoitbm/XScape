27a524b7dbd80bad2c112d94ed8f12a2343e6ac5
Benoit Boyer <contact@benoitbm.fr>
2017-03-02 01:12:21 +0100

    Initial commit

---
+++ Source/XScape/XScapeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "XScapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XSCAPE_API AXScapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
